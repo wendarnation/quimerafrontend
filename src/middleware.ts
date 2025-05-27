68088e1497126ffa4118762311a80a89065bfe60
Carlos Cortés <c.cortescandela@gmail.com>
2025-05-27 16:13:51 +0200

    Mejoras produccion

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
 import type { NextRequest } from "next/server";
 import { auth0 } from "./lib/auth0";
 
+//
+
 export async function middleware(request: NextRequest) {
   return await auth0.middleware(request);
 }
